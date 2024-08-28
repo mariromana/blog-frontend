07d84181ff38d243ce1e1252bcaa1c7bb2bf5b88
mari <mariroman604@gmail.com>
2024-08-28 18:29:46 +0200

    change jest config

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,13 @@
-module.exports = {
-    transform: {
-        '^.+\\.jsx?$': 'babel-jest',
-    },
-    testEnvironment: 'jsdom',
-    // transformIgnorePatterns: ['/node_modules/(?!axios)'],
-    transformIgnorePatterns: [
-        'node_modules/(?!react-markdown|vfile|unist-util-stringify-position|property-information|space-separated-tokens|comma-separated-tokens|zwitch|unist-util-visit|unist-util-is|hast-util-whitespace)',
-    ],
-    moduleNameMapper: {
-        'react-markdown': 'react-markdown/react-markdown.min.js',
-    },
-};
+// module.exports = {
+//     transform: {
+//         '^.+\\.jsx?$': 'babel-jest',
+//     },
+//     testEnvironment: 'jsdom',
+//     // transformIgnorePatterns: ['/node_modules/(?!axios)'],
+//     transformIgnorePatterns: [
+//         'node_modules/(?!react-markdown|vfile|unist-util-stringify-position|property-information|space-separated-tokens|comma-separated-tokens|zwitch|unist-util-visit|unist-util-is|hast-util-whitespace)',
+//     ],
+//     moduleNameMapper: {
+//         'react-markdown': 'react-markdown/react-markdown.min.js',
+//     },
+// };
